da6d514c71ac3f62fff57e5fe03e46030e4dbdb6
Igor <33908689+igorishere@users.noreply.github.com>
2019-07-17 23:04:07 -0300

    Toolbar at footer contain data about the last saved item add

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
